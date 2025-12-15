0f729ee0acfe1ae0e234e72738309ec770bf0ea6
wpeterashworth <wingsaseagles12@gmail.com>
2025-12-15 13:08:37 -0700

    fixed a couple styling issues

--- public/scripts/dates.js
+++ public/scripts/dates.js
@@ -1,8 +1,15 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
+// const currentDate = new Date();
+// const currentYear = currentDate.getFullYear();
 
-let year = document.querySelector("#date");
-year.innerHTML = `&copy${currentYear}`;
+// let year = document.querySelector("#date");
+// year.innerHTML = `&copy${currentYear}`;
 
-let lastModifiedDate = document.querySelector("#lastModified");
-lastModifiedDate.textContent = new Date(document.lastModified);
+// let lastModifiedDate = document.querySelector("#lastModified");
+// lastModifiedDate.textContent = new Date(document.lastModified);
+
+let menuButton = document.querySelector("#menu");
+let navList = document.querySelector("nav ul")
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open")
+    navList.classList.toggle("open")
+})
